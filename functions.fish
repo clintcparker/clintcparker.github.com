713f9424fa92603363a7bf816ac1b13aa2725225
Clint Parker <me@clintparker.com>
2026-03-20 11:20:20 -0700

    updated deploy to work with codespaces

--- functions.fish
+++ functions.fish
@@ -4,8 +4,11 @@ end
 
 function od --description 'Deploy an octopress site in production configuration'
     rm -rf /workspaces/clintcparker.github.com/.deploy
-	set -gx JEKYLL_ENV 'production'
+    set -gx JEKYLL_ENV 'production'
     jekyll build
+    if set -q GITHUB_TOKEN
+        git -C .deploy remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/clintcparker/clintcparker.github.com.git"
+    end
     octopress deploy
     set -gx JEKYLL_ENV 'development'
 end
